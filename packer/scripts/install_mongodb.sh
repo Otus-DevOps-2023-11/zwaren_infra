c5b87a1f1136dce9c96530ba3f8b2bf4404162d0
Ilya Kotov <zwaren@yandex.ru>
2024-05-14 01:41:41 +0300

    Refactoring terraform config

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -x
 sudo apt update
-DEBIAN_FRONTEND=noninteractive sudo apt install mongodb -y
+sleep 30
+sudo apt install mongodb -y
 systemctl status mongodb
 sudo systemctl enable mongodb
